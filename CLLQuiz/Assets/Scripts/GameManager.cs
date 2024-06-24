bdd06e9fa50d0e7cf6d55e2a2d9863ec16ba3b7a
Changou <117820115+Changou@users.noreply.github.com>
2024-06-24 17:26:12 +0900

    CLLQuiz 완료

--- CLLQuiz/Assets/Scripts/GameManager.cs
+++ CLLQuiz/Assets/Scripts/GameManager.cs
@@ -19,12 +19,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] Text _TurnCount;
     [SerializeField] GameObject _TurnBtn;
     [SerializeField] GameObject _StopBtn;
-    [SerializeField] Text _GameOverT;
+    [SerializeField] GameObject _GameOver;
+    [SerializeField] GameObject _ReBtn;
+
+    [Header("도전횟수")]
+    [SerializeField] int _TurnCnt = 2;
+
+    [Header("당첨")]
+    [SerializeField] GameObject _Winning;
+    
 
     bool _isTurn = false;
     bool _isTurnOver = true;
     float point = 0;
-    [SerializeField] int _TurnCnt = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -43,19 +50,46 @@ public class GameManager : MonoBehaviour
     {
         CLList<GameObject>.CircleNode curNode = _list._CurNode;
         point += curNode._data.GetComponent<RulletObj>()._Point;
+
         _ScoreT.text = "점수 : " + point;
+        StartCoroutine(WinningDelay((int)curNode._data.GetComponent<RulletObj>()._Point));
+    }
+
+    IEnumerator WinningDelay(int winningPoint)
+    {
+        _Winning.SetActive(true);
+        _Winning.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = winningPoint + "점에 당첨되었습니다.";
+        if (winningPoint > 0)
+            _Winning.transform.GetChild(0).gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(3f);
+
+        _Winning.transform.GetChild(0).gameObject.SetActive(false);
+        _Winning.SetActive(false);
         _isTurnOver = true;
-        _TurnBtn.SetActive(true);
-        if(_TurnCnt == 0)
-        {
+        
+        if (_TurnCnt == 0)
             GameOver();
-        }
+        else
+            _TurnBtn.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        _ReBtn.SetActive(false);
+        _GameOver.SetActive(false);
+        point = 0;
+        _TurnCnt = 5;
+        _ScoreT.text = "점수 : " + point;
+        _TurnCount.text = "남은 횟수 : " + _TurnCnt;
+        _TurnBtn.SetActive(true) ;
     }
 
     void GameOver()
     {
-        _GameOverT.gameObject.SetActive(true);
-        _GameOverT.text = "GAME OVER\n점수 : " + point;
+        _GameOver.SetActive(true);
+        _GameOver.transform.GetChild(0).GetComponent<Text>().text = "GAME OVER\n점수 : " + point;
+        _ReBtn.SetActive(true);
     }
 
     public void OffTurnBtn()
@@ -82,12 +116,12 @@ public class GameManager : MonoBehaviour
     IEnumerator StopPointChoose()
     {
         float _stopSpeed = _Rspeed;
-        while(_stopSpeed <= 2f)
+        while(_stopSpeed <= 1.5f)
         {
             _list.SetCurNodeToNext();
             ShowCurNode();
             yield return new WaitForSeconds(_stopSpeed);
-            _stopSpeed += 0.2f;
+            _stopSpeed += 0.3f;
         }
         Score();
     }
